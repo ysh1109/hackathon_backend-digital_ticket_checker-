ce82c956c614ec076d3db8b3f8576ec54274bc3d
Swapnil Goyal <swapnil.goyal15@gmail.com>
2018-03-27 15:20:24 +0000

    added static directory and hosts

--- LogicView/settings.py
+++ LogicView/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=k+s!o-($o8ua!o=su574coabv33_h8yl-w_3)o6(2v0i(=f6l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['159.89.163.178', '159.89.163.178:8000', 'localhost', '127.0.0.1:8000']
 
 
 # Application definition
@@ -118,5 +118,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
